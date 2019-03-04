d20413406dbb31e5e6916d13fe9b7d1ad44f54fc
Thomas Dornell Bonilla <thomasdornell@gmail.com>
2019-03-04 22:24:32 +0100

    Updates

---
+++ precache-manifest.699b4846dd3b2ad45442212cea027b60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f8bca70917496775ddfb",
+    "url": "/book-finder/static/css/main.860e5f1e.chunk.css"
+  },
+  {
+    "revision": "f8bca70917496775ddfb",
+    "url": "/book-finder/static/js/main.f8bca709.chunk.js"
+  },
+  {
+    "revision": "736fd4229ce5946055cf",
+    "url": "/book-finder/static/js/runtime~main.736fd422.js"
+  },
+  {
+    "revision": "c84607e0f73c719a88c7",
+    "url": "/book-finder/static/css/2.a7b31c20.chunk.css"
+  },
+  {
+    "revision": "c84607e0f73c719a88c7",
+    "url": "/book-finder/static/js/2.c84607e0.chunk.js"
+  },
+  {
+    "revision": "e95c6d392c553f66a0b89f07db762eb9",
+    "url": "/book-finder/index.html"
+  }
+];
